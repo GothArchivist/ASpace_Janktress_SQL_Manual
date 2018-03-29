417ec9b62979b323ed83c52a03917367e3c9bb9a
Christy <christy.tomecek@yale.edu>
2018-03-29 09:42:31 -0400

    Update MultipleAgentsToResource.sql

--- Agents/MultipleAgentsToResource.sql
+++ Agents/MultipleAgentsToResource.sql
@@ -1,8 +1,8 @@
-SELECT resource.title, resource.id, resource.ead_id, nce.primary_name, nce.agent_corporate_entity_id, lar.role_id, enumeration_value.value AS role
+SELECT resource.title, resource.ead_id, nce.primary_name, lar.agent_corporate_entity_id, lar.role_id, enumeration_value.value AS role
 FROM linked_agents_rlshp lar
 LEFT JOIN resource ON lar.resource_id=resource.id
 LEFT JOIN name_corporate_entity nce ON lar.agent_corporate_entity_id=nce.agent_corporate_entity_id
 LEFT JOIN enumeration_value ON lar.role_id=enumeration_value.id
-WHERE nce.primary_name IN ('Fortunoff Video Archive for Holocaust Testimonies','United States Holocaust Memorial Museum')
+WHERE nce.primary_name IN ('agent 1','agent 2') #Don't forget to swap in your actual agent names! Put in as many as you need.
 GROUP BY lar.resource_id
-HAVING COUNT(lar.resource_id)=2
+HAVING COUNT(lar.resource_id)=2 #Increase count by the amount of agents you have listed above.
