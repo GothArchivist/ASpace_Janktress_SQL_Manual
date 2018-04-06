d36a13eb7d9bafc0f64cfdec77098f57a985f778
Christy <christy.tomecek@yale.edu>
2018-04-06 11:21:47 -0400

    Update and rename get_restriction_info to get_restriction_info_ao.sql

---
+++ Restrictions/get_restriction_info_ao.sql
@@ -0,0 +1,7 @@
+SELECT rr.archival_object_id, rr.restriction_note_type, rr.begin, rr.end, note.notes, npi.persistent_id, rrt.restriction_type_id, enumeration_value.value
+FROM rights_restriction rr
+LEFT JOIN note on rr.archival_object_id=note.archival_object_id
+LEFT JOIN note_persistent_id npi on note.id=npi.note_id
+LEFT JOIN rights_restriction_type rrt on rr.id=rrt.rights_restriction_id
+LEFT JOIN enumeration_value on rrt.restriction_type_id=enumeration_value.id
+WHERE rr.archival_object_id IN () #insert IDs here
