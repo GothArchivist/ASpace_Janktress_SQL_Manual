b151669581ceeec6fba71487b33e17635de88317
Christy <christy.tomecek@yale.edu>
2018-04-06 14:55:42 -0400

    Update get_restriction_info_ao.sql

--- Restrictions/get_restriction_info_ao.sql
+++ Restrictions/get_restriction_info_ao.sql
@@ -1,4 +1,5 @@
 SELECT rr.archival_object_id, rr.restriction_note_type, rr.begin, rr.end, note.notes, npi.persistent_id, rrt.restriction_type_id, enumeration_value.value
+# , CAST(note.notes as CHAR (10000) CHARACTER SET UTF8) AS restriction_text
 FROM rights_restriction rr
 LEFT JOIN note on rr.archival_object_id=note.archival_object_id
 LEFT JOIN note_persistent_id npi on note.id=npi.note_id
